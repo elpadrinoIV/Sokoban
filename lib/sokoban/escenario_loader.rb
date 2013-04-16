0888729e7e02909eaeedd643a2063475bbc1ae7d
Nicolas <nicolasdascanio@gmail.com>
2013-04-16 15:10:12 -0300

    carga de niveles a partir de archivos listo

--- lib/sokoban/escenario_loader.rb
+++ lib/sokoban/escenario_loader.rb
@@ -22,6 +22,8 @@ class EscenarioLoader
   def cargar_escenario lineas
     escenario = Escenario.new
     fila = 0
+    min_fila = 1000000
+    min_col = 1000000
     lineas.each do |linea|
       linea = strip_comentarios linea
       if !linea_en_blanco?(linea)
@@ -34,20 +36,32 @@ class EscenarioLoader
             pared = Pared.new
             pared.set_posicion(columna, fila)
             escenario.agregar_pared(pared)
+
+            min_col = columna if columna < min_col
+            min_fila = fila if fila < min_fila
           when '.'
             target = Destino.new
             target.set_posicion(columna, fila)
             escenario.agregar_destino(target)
+
+            min_col = columna if columna < min_col
+            min_fila = fila if fila < min_fila
           when '$'
             caja = Caja.new
             caja.set_posicion(columna, fila)
             caja.set_escenario(escenario)
             escenario.agregar_caja(caja)
+
+            min_col = columna if columna < min_col
+            min_fila = fila if fila < min_fila
           when '@'
             persona = Persona.new
             persona.set_posicion(columna, fila)
             persona.set_escenario(escenario)
             escenario.agregar_persona(persona)
+
+            min_col = columna if columna < min_col
+            min_fila = fila if fila < min_fila
           when '*'
             target = Destino.new
             target.set_posicion(columna, fila)
@@ -57,20 +71,27 @@ class EscenarioLoader
             caja.set_posicion(columna, fila)
             caja.set_escenario(escenario)
             escenario.agregar_caja(caja)
+
+            min_col = columna if columna < min_col
+            min_fila = fila if fila < min_fila
           when '+'
             target = Destino.new
-            target.set_posicion(fila, columna)
+            target.set_posicion(columna, fila)
             escenario.agregar_destino(target)
-
+            
             persona = Persona.new
-            persona.set_posicion(fila, columna)
+            persona.set_posicion(columna, fila)
             persona.set_escenario(escenario)
             escenario.agregar_persona(persona)
+
+            min_col = columna if columna < min_col
+            min_fila = fila if fila < min_fila
           end
         end
       end
     end
 
+    shift_escenario escenario, min_fila, min_col
     escenario
   end
 
@@ -82,6 +103,28 @@ class EscenarioLoader
   def strip_comentarios linea
     linea = linea.split(';')[0]
     linea
+  end
+
+  def shift_escenario escenario, min_fila, min_col
+    shift_x = min_col - 1
+    shift_y = min_fila - 1
+
+    if 0 != shift_x || 0 != shift_y
+      escenario.get_paredes.each do |pared|
+        pared.set_posicion(pared.get_x - shift_x, pared.get_y - shift_y)
+      end
+
+      escenario.get_cajas.each do |caja|
+        caja.set_posicion(caja.get_x - shift_x, caja.get_y - shift_y)
+      end
+
+      escenario.get_destinos.each do |destino|
+        destino.set_posicion(destino.get_x - shift_x, destino.get_y - shift_y)
+      end
+
+      escenario.get_persona.set_posicion(escenario.get_persona.get_x - shift_x, escenario.get_persona.get_y - shift_y)
+
+    end
   end
 
 
--- test/test_06A_cargar_niveles.rb
+++ test/test_06A_cargar_niveles.rb
@@ -12,23 +12,178 @@ class TestCargarNiveles < Test::Unit::TestCase
     # ####
     # #. #
     # #$@#
-    # ####
+    #  ###
 
     archivo = File.dirname(__FILE__) + '/esquema_test_06A01_sencillo.txt'
     escenario = @escenario_loader.cargar_escenario_desde_archivo(archivo)
 
+    check_primeras_pruebas escenario
+  end
+
+  def test_06a02_carga_sencilla_trailing_space
+    # ####
+    # #. #
+    # #$@#
+    #  ###
+
+    archivo = File.dirname(__FILE__) + '/esquema_test_06A02_sencillo_trailing_space.txt'
+    escenario = @escenario_loader.cargar_escenario_desde_archivo(archivo)
+
+    check_primeras_pruebas escenario
+  end
+
+  def test_06a03_carga_sencilla_espacios_antes_y_despues
+    # ####
+    # #. #
+    # #$@#
+    #  ###
+
+    archivo = File.dirname(__FILE__) + '/esquema_test_06A02_sencillo_espacios_antes_y_despues.txt'
+    escenario = @escenario_loader.cargar_escenario_desde_archivo(archivo)
+
+    check_primeras_pruebas escenario
+  end
+
+  def test_06a04_carga_compleja
+    #    ### ##
+    #   #. ###*#
+    #   #$ $ + #
+    #    ######
+    archivo = File.dirname(__FILE__) + '/esquema_test_06A04_complejo.txt'
+    escenario = @escenario_loader.cargar_escenario_desde_archivo(archivo)
+
+
+    pared_en_2_1 = false
+    pared_en_3_1 = false
+    pared_en_4_1 = false
+    pared_en_6_1 = false
+    pared_en_7_1 = false
+
+    pared_en_1_2 = false
+    pared_en_4_2 = false
+    pared_en_5_2 = false
+    pared_en_6_2 = false
+    pared_en_8_2 = false
+
+    pared_en_1_3 = false
+    pared_en_8_3 = false
+
+    pared_en_2_4 = false
+    pared_en_3_4 = false
+    pared_en_4_4 = false
+    pared_en_5_4 = false
+    pared_en_6_4 = false
+    pared_en_7_4 = false
+
+    destino_en_2_2 = false
+    destino_en_7_2 = false
+    destino_en_6_3 = false
+
+    caja_en_7_2 = false
+    caja_en_2_3 = false
+    caja_en_4_3 = false
+
+    persona_en_6_3 = false
+
+    escenario.get_paredes.each do |pared|
+      if 2 == pared.get_x && 1 == pared.get_y
+        pared_en_2_1 = true
+      elsif 3 == pared.get_x && 1 == pared.get_y
+        pared_en_3_1 = true
+      elsif 4 == pared.get_x && 1 == pared.get_y
+        pared_en_4_1 = true
+      elsif 6 == pared.get_x && 1 == pared.get_y
+        pared_en_6_1 = true
+       elsif 7 == pared.get_x && 1 == pared.get_y
+        pared_en_7_1 = true
+      elsif 1 == pared.get_x && 2 == pared.get_y
+        pared_en_1_2 = true
+      elsif 4 == pared.get_x && 2 == pared.get_y
+        pared_en_4_2 = true
+      elsif 5 == pared.get_x && 2 == pared.get_y
+        pared_en_5_2 = true
+      elsif 6 == pared.get_x && 2 == pared.get_y
+        pared_en_6_2 = true
+      elsif 8 == pared.get_x && 2 == pared.get_y
+        pared_en_8_2 = true
+      elsif 1 == pared.get_x && 3 == pared.get_y
+        pared_en_1_3 = true
+      elsif 8 == pared.get_x && 3 == pared.get_y
+        pared_en_8_3 = true
+      elsif 2 == pared.get_x && 4 == pared.get_y
+        pared_en_2_4 = true
+      elsif 3 == pared.get_x && 4 == pared.get_y
+        pared_en_3_4 = true
+      elsif 4 == pared.get_x && 4 == pared.get_y
+        pared_en_4_4 = true
+      elsif 5 == pared.get_x && 4 == pared.get_y
+        pared_en_5_4 = true
+      elsif 6 == pared.get_x && 4 == pared.get_y
+        pared_en_6_4 = true
+      elsif 7 == pared.get_x && 4 == pared.get_y
+        pared_en_7_4 = true
+      end
+    end
+
+    paredes_bien = pared_en_2_1 && pared_en_3_1 && pared_en_4_1 && pared_en_6_1 && pared_en_7_1 &&
+                   pared_en_1_2 && pared_en_4_2 && pared_en_5_2 && pared_en_6_2 && pared_en_8_2 &&
+                   pared_en_1_3 && pared_en_8_3 &&
+                   pared_en_2_4 && pared_en_3_4 && pared_en_4_4 && pared_en_5_4 && pared_en_6_4 && pared_en_7_4
+
+    paredes_bien = paredes_bien && (18 == escenario.get_paredes.length)
+
+    escenario.get_destinos.each do |destino|
+      if 2 == destino.get_x && 2 == destino.get_y
+        destino_en_2_2 = true
+      elsif 7 == destino.get_x && 2 == destino.get_y
+        destino_en_7_2 = true
+      elsif 6 == destino.get_x && 3 == destino.get_y
+        destino_en_6_3 = true
+      end
+    end
+
+    destinos_bien = destino_en_2_2 && destino_en_7_2 && destino_en_6_3 && (3 == escenario.get_destinos.length)
+
+    escenario.get_cajas.each do |caja|
+      if 7 == caja.get_x && 2 == caja.get_y
+        caja_en_7_2 = true
+      elsif 2 == caja.get_x && 3 == caja.get_y
+        caja_en_2_3 = true
+      elsif 4 == caja.get_x && 3 == caja.get_y
+        caja_en_4_3 = true
+      end
+    end
+
+    cajas_bien = caja_en_7_2 && caja_en_2_3 && caja_en_4_3 && (3 == escenario.get_cajas.length)
+
+    persona_en_6_3 = (6 == escenario.get_persona.get_x && 3 == escenario.get_persona.get_y)
+
+    assert_equal(true, paredes_bien, "Mal la carga de paredes a partir del archivo")
+    assert_equal(true, destinos_bien, "Mal la carga de destinos a partir del archivo")
+    assert_equal(true, cajas_bien, "Mal la carga de cajas a partir del archivo")
+    assert_equal(true, persona_en_6_3, "Mal la carga de la persona a partir del archivo")
+    assert_equal(8, escenario.get_ancho, "Mal el ancho")
+    assert_equal(4, escenario.get_alto, "Mal el alto")
+  end
+
+  # como todos los primeros 3 tests se hace todo igual salvo el archivo que se carga, lo agrupo aca
+  def check_primeras_pruebas escenario
+    # ####
+    # #. #
+    # #$@#
+    # ####
+    
     pared_en_1_1 = false
     pared_en_2_1 = false
     pared_en_3_1 = false
     pared_en_4_1 = false
-
+    
     pared_en_1_2 = false
     pared_en_4_2 = false
 
     pared_en_1_3 = false
     pared_en_4_3 = false
 
-    pared_en_1_4 = false
     pared_en_2_4 = false
     pared_en_3_4 = false
     pared_en_4_4 = false
@@ -54,8 +209,6 @@ class TestCargarNiveles < Test::Unit::TestCase
         pared_en_1_3 = true
       elsif 4 == pared.get_x && 3 == pared.get_y
         pared_en_4_3 = true
-      elsif 1 == pared.get_x && 4 == pared.get_y
-        pared_en_1_4 = true
       elsif 2 == pared.get_x && 4 == pared.get_y
         pared_en_2_4 = true
       elsif 3 == pared.get_x && 4 == pared.get_y
@@ -65,12 +218,12 @@ class TestCargarNiveles < Test::Unit::TestCase
       end
     end
 
-    paredes_bien = pared_en_1_1 && pared_en_2_1 && pared_en_3_1 && pared_en_4_1 &&
+    paredes_bien = pared_en_1_1 && pared_en_2_1 && pared_en_3_1 && pared_en_4_1
                    pared_en_1_2 && pared_en_4_2 && pared_en_1_3 && pared_en_4_3 &&
-                   pared_en_1_4 && pared_en_2_4 && pared_en_3_4 && pared_en_4_4
+                                   pared_en_2_4 && pared_en_3_4 && pared_en_4_4
 
-    paredes_bien = paredes_bien && (12 == escenario.get_paredes.length)
-
+    paredes_bien = paredes_bien && (11 == escenario.get_paredes.length)
+    
     escenario.get_destinos.each do |destino|
       if 2 == destino.get_x && 2 == destino.get_y
         destino_en_2_2 = true
@@ -96,7 +249,6 @@ class TestCargarNiveles < Test::Unit::TestCase
     assert_equal(4, escenario.get_ancho, "Mal el ancho")
     assert_equal(4, escenario.get_alto, "Mal el alto")
   end
-
 
 
 end
